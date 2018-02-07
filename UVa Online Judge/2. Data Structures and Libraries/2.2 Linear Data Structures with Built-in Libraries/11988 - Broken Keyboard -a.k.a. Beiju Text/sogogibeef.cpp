eeb417b0d3bc7a9a9daa2c8b550c0db9fe647a69
sogogibeef <limjeongmin@wustl.edu>
2018-02-08 01:12:50 +0900

    Update sogogibeef.cpp
    http://www.cplusplus.com/reference/list/list/insert/

--- UVa Online Judge/2. Data Structures and Libraries/2.2 Linear Data Structures with Built-in Libraries/11988 - Broken Keyboard -a.k.a. Beiju Text/sogogibeef.cpp
+++ UVa Online Judge/2. Data Structures and Libraries/2.2 Linear Data Structures with Built-in Libraries/11988 - Broken Keyboard -a.k.a. Beiju Text/sogogibeef.cpp
@@ -1,9 +1,8 @@
-
 //#include <bits/stdc++.h>
 #include "/Users/jeongminlim/stdc++.h"
 
 using namespace std;
-bool front = false;
+
 
 int main() {
 #ifndef ONLINE_JUDGE
@@ -12,42 +11,41 @@ int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(0);
     //code here
-    string line;
-    string text, b, f;
+    // move pointer to the beginning of list after encountering '['
+    string input;
+    while (cin >> input) {
+        list<char> beiju;
+        beiju.clear();
+        auto position = beiju.begin();
 
-    while (cin >> line) {
-        text.clear();
-        b.clear();
-        f.clear();
-
-        for (int i = 0; i < line.size(); i++) {
-            if (line[i] == '[') {
-                front = true;
-                reverse(f.begin(), f.end());
-                text = f + text;
-                f.clear();
+        for (int i = 0; i < input.size(); i++) {
+            if (input[i] == '[') {
+                position = beiju.begin();
                 continue;
 
-            } else if (line[i] == ']') {
-                front = false;
-                reverse(f.begin(), f.end());
-                text = f + text;
-                f.clear();
+            } else if (input[i] == ']') {
+                position = beiju.end();
                 continue;
             }
 
-            if (front) {
-                f = line[i] + f;
-            } else {
-                text = text + line[i];
-            }
+            beiju.insert(position, input[i]);
 
         }
-        reverse(f.begin(), f.end());
-        text = f + text;
-        cout << text << "\n";
+
+
+        for (auto &i:beiju) {
+            cout << i;
+        }
+        cout << "\n";
+
+
     }
 
 
     return 0;
 }
+
+
+
+
+
